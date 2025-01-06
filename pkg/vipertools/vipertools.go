604df3a996f2bf7dcf5031b6d85d5f15af72ec79
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2025-01-06 16:35:15 -0300

    Refactor FirstNonEmptyBool to accept false values

--- pkg/vipertools/vipertools.go
+++ pkg/vipertools/vipertools.go
@@ -8,23 +8,32 @@ import (
 )
 
 // FirstNonEmptyBool accepts multiple keys and returns the first non-empty bool value
-// from viper.Viper via these keys. Non-empty meaning false value will not be accepted.
+// from viper.Viper via these keys. Non-empty meaning key not set will not be accepted.
 func FirstNonEmptyBool(v *viper.Viper, keys ...string) bool {
 	if v == nil {
 		return false
 	}
 
 	for _, key := range keys {
-		if value := v.GetBool(key); value {
-			return value
+		if !v.IsSet(key) {
+			continue
 		}
+
+		value := v.Get(key)
+
+		parsed, err := cast.ToBoolE(value)
+		if err != nil {
+			continue
+		}
+
+		return parsed
 	}
 
 	return false
 }
 
 // FirstNonEmptyInt accepts multiple keys and returns the first non-empty int value
-// from viper.Viper via these keys. Non-empty meaning 0 value will not be accepted.
+// from viper.Viper via these keys. Non-empty meaning key not set will not be accepted.
 // Will return false as second parameter, if non-empty int value could not be retrieved.
 func FirstNonEmptyInt(v *viper.Viper, keys ...string) (int, bool) {
 	if v == nil {
@@ -59,6 +68,19 @@ func FirstNonEmptyString(v *viper.Viper, keys ...string) string {
 	}
 
 	for _, key := range keys {
+		// if !v.IsSet(key) {
+		// 	continue
+		// }
+
+		// value := v.Get(key)
+
+		// parsed, err := cast.ToStringE(value)
+		// if err != nil {
+		// 	continue
+		// }
+
+		// return parsed
+
 		if value := GetString(v, key); value != "" {
 			return value
 		}
